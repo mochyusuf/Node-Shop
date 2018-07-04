ab4234567dc779a01993e4ab95c43cc15fcaf22e
Mochamad Yusuf <mochyusuf100@gmail.com>
2018-07-04 13:41:39 +0700

    Episode 9 Sign In

--- config/passport.js
+++ config/passport.js
@@ -39,10 +39,39 @@ passport.use('local.signup',new LocalStrategy({
             newUser.password = newUser.encryptPassword(password);
             newUser.save(function(err, result){
                 if (err) {
-                    return done();                    
+                    return done(err);                    
                 }
                 return done(null, newUser);
             })
         })
     }
 ));
+
+passport.use('local.signin',new LocalStrategy({
+    usernameField:'email',
+    passwordField:'password',
+    passReqToCallback:true
+}, function(req, email, password, done){
+    req.checkBody('email','Invalid Email').notEmpty().isEmail();
+    req.checkBody('password','Invalid password').notEmpty().isLength({min:4});
+    var errors = req.validationErrors();
+    if (errors) {
+        var messages = [];
+        errors.forEach(function(errors){
+            messages.push(errors.msg);
+        })
+        return done(null,false,req.flash('error',messages));
+    }
+    User.findOne({'email': email}, function(err, user){
+        if(err){
+            return done(err);
+        }
+        if(!user){
+            return done(null, false, {message: "Email Tidak Ditemukan"});
+        }
+        if (!user.validPassword(password)) {
+            return done(null, false, {message: "Password Salah"});
+        }
+        return done(null, user);
+    })
+}))
