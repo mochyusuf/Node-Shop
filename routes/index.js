6a9224bf57214e332a0376061f9aeba28be8382d
Mochamad Yusuf <mochyusuf100@gmail.com>
2018-07-04 10:01:36 +0700

    Episode 5

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var Produk = require('../models/produk');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shop' });
+  Produk.find(function(err,docs){
+    var produkChunks = [];
+    var chunkSize = 3;
+    for (let i = 0; i < docs.length; i+= chunkSize) {
+      produkChunks.push(docs.slice(i, i + chunkSize));
+      
+    }
+    res.render('shop/index', { title: 'Shop' , produks: produkChunks});
+  });
 });
 
 module.exports = router;
